a58907f2fae9c97f583f998024de7ad3a7e27141
Equipe2devweb <138901361+Equipe2devweb@users.noreply.github.com>
2023-07-26 14:04:13 -0400

    Delete TestProjectKidsGames-main/protjectPHP directory

--- TestProjectKidsGames-main/protjectPHP/py/sort.py
+++
@@ -1,8 +0,0 @@
-def sort_string(string):
-    sorted_string = ''.join(sorted(string, key=lambda x: x.lower()))
-    return sorted_string
-
-# Example usage
-input_string = "eghdHt"
-sorted_string = sort_string(input_string)
-print(sorted_string)
